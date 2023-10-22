fa381961442231d14a381b5850707b13c10b0db5
dwiindahr <147695065+dwiindahr@users.noreply.github.com>
2023-10-22 12:04:14 +0700

    Update landingPageEts.js

--- landingPageEts.js
+++ landingPageEts.js
@@ -28,9 +28,9 @@ const sr = ScrollReveal({
 sr.reveal('.hero-text', {origin:'top'});
 sr.reveal('.about-img, .service-item, .about-text', {origin:'bottom'});
 sr.reveal('.about-text h2, .text-center, .right-contact h2', {origin:'top'});
-sr.reveal('.left-contact', {origin:'left'});
-sr.reveal('.right-contact', {origin:'right'});
-sr.reveal('.end-section', {origin:'top'});
+sr.reveal('.left-hire', {origin:'left'});
+sr.reveal('.right-hire', {origin:'right'});
+sr.reveal('.contact', {origin:'top'});
 
 let slideIndex = 1;
 showSlides(slideIndex);
